7b948781d90fa8382711fb60a8813d8a2a1f8b76
TootimIUT <gouasmier.alexandre@gmail.com>
2022-03-30 09:19:50 +0200

    Ajout tetris.xaml edit registration.xaml et login.xaml

---
+++ projet_tetris/view/Registration.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace projet_tetris.model
+{
+    /// <summary>  
+    /// Interaction logic for Registration.xaml  
+    /// </summary>  
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            Close();
+        }
+        
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (textBoxFirstName.Text.Length == 0)
+            {
+                errormessage.Text = "Entrer un pseudo.";
+                textBoxEmail.Focus();
+            }
+
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Entrer un mail.";
+                textBoxEmail.Focus();
+            }
+
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Entrer un mail valide.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string firstname = textBoxFirstName.Text;
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                if (passwordBox1.Password.Length == 0)
+                {
+                    errormessage.Text = "Entrer un mot de passe.";
+                    passwordBox1.Focus();
+                }
+                else if (passwordBoxConfirm.Password.Length == 0)
+                {
+                    errormessage.Text = "Confirmer le mot de passe.";
+                    passwordBoxConfirm.Focus();
+                }
+                else if (passwordBox1.Password != passwordBoxConfirm.Password)
+                {
+                    errormessage.Text = "Les mots de passe ne correspondent pas.";
+                    passwordBoxConfirm.Focus();
+                }
+                else
+                {
+                    errormessage.Text = "";
+                    /*SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Insert into Registration (FirstName,LastName,Email,Password,Address) values('" + firstname + "','" + email + "','" + password + "')", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    errormessage.Text = "Vous etes inscris ! Bienvenue.";*/
+                }
+            }
+        }
+    }
+}
